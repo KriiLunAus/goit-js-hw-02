ce7320afc1735e48309081844a41ab7bcfcc1b07
KriiLunAus <kriilunaus16@gmail.com>
2023-11-20 17:43:02 +0100

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -1,6 +1,13 @@
-
-function toggleModalVisibility(isVisible) {
-  console.log(!isVisible);
+function normalizeInput(input, to) {
+  if (to === "upper") {
+    console.log(input.toUpperCase());
+  }else{
+    console.log(input.toLowerCase());
+  }
 }
-toggleModalVisibility(true)
-toggleModalVisibility(false)
+normalizeInput("This ISN'T SpaM", "upper")
+normalizeInput("Big SALE", "lower")
+normalizeInput("This ISN'T SpaM", "lower")
+normalizeInput("Big SALE", "upper")
+normalizeInput("Stay Awhile and Listen", "lower")
+ normalizeInput("Stay Awhile and Listen", "upper")
