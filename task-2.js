37ef6354dee0579f330ea191da4e7940ceace4ad
KriiLunAus <kriilunaus16@gmail.com>
2023-12-03 12:35:55 +0100

    Task 3,4

--- task-2.js
+++ task-2.js
@@ -1,4 +1,5 @@
-function formatMessage(message, maxLength){
+`use strict`
+function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message;
     } else {
---
+++ task-3.js
@@ -0,0 +1,29 @@
+`use strict`
+function checkForSpam(message) {
+    message = message.toLowerCase();
+    const hasSpam = message.includes('spam') || message.includes('sale');
+    if (hasSpam) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!")); 
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+
+
+
+
+
+
+
+
+
