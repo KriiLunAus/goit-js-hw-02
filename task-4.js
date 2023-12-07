17bcb7e7f0d462504881551dd666a00b2e8b1e11
KriiLunAus <kriilunaus16@gmail.com>
2023-12-07 18:42:01 +0100

    w

--- task-4.js
+++ task-4.js
@@ -1,29 +1,5 @@
-`use strict`
-function getShippingCost(country) {
-    switch (country) {
-        case 'China':
-            console.log(`Shipping to ${country} cost 100 credits`)
-            break;
-        case 'Chile':
-            console.log(`Shipping to ${country} will cost 250 credits`)
-            break;
-        case 'Australia': 
-            console.log(`Shipping to ${country} will cost 170 credits`)
-            break;
-        case 'jamaica':
-            console.log(`Shipping to ${country} will cost 120 credits`)
-            break;
-        default:
-            console.log(`Sorry, there is no delivery to your country`)
-    }
+{
+const name="Mango"
+console.log(name) // "Mango"
 }
-
-
-getShippingCost("Australia")
-getShippingCost("Germany")
-getShippingCost("China")
-getShippingCost("Chile")
-getShippingCost("Jamaica")
-getShippingCost("Sweden")
-
-
+console.log(name) // значення глобальної змінної name. Помилки в консолі не буде
